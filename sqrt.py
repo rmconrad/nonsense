cee48596f305710fe6c1a557b92d64bb46098049
Ryan Conrad <rmconrad93@gmail.com>
2017-06-01 18:28:41 -0400

    made sqrt work

--- sqrt.py
+++ sqrt.py
@@ -1,10 +1,25 @@
-import math
-
 def find_next_square(sq):
     #Return the next square if sq is a square, -1 otherwise
-    if (math.sqrt(sq) % math.sqrt(sq)) != 1:
+    root = sq ** 0.5
+    if root.is_integer():
+        return (root + 1) ** 2
+    else:
         return -1
-    else n = ((math.sqrt(sq) + 1) ** 2):
-        return n
 
-find_next_square(121)
+
+# Dewey's Version:
+#
+# #! /path/to/python
+#
+# import math
+# import sys
+#
+# dude = float(sys.argv[1])
+#
+# def perfect_sq():
+#         n = dude
+#         if n == int(math.sqrt(n)) ** 2:
+#                 print “square”
+#         else:
+#                 print “not a square”
+# perfect_sq()
